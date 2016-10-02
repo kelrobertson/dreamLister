53ba47389bbdcd7d23cf0d3be5ada5bfc4bac094
kelrobertson <kel.robertson87@gmail.com>
2016-10-02 14:08:26 +1000

    Updated UI, added outlets and boiler plater code ready for coreData use

---
+++ dreamlister2/itemCell.swift
@@ -0,0 +1,19 @@
+//
+//  itemCell.swift
+//  dreamlister2
+//
+//  Created by Kel Robertson on 2/10/2016.
+//  Copyright © 2016 Kel Robertson. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+    @IBOutlet weak var thumbnail: UIImageView!
+
+}
